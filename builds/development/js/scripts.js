286ca2505086915f3df8b509c351f259524dbaa5
GraemeBingham <binghamgraeme@gmail.com>
2016-06-06 18:29:10 +1000

    Jquery for FadeIn elements on index.html

--- builds/development/js/scripts.js
+++ builds/development/js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-  $("#home").fadeIn('slow');
 
+  // Scripts for Index.html
+  $('.container').fadeIn('4000',function(){
+    $('.stage').fadeIn('4000').removeClass('hidden');
+  }).removeClass('hidden');
+
+
+
+  // Scripts for page1.html
 });
