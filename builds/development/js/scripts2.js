1f4b82465b28692fc4c74346419803ff570ffab1
GraemeBingham <binghamgraeme@gmail.com>
2016-06-07 09:51:09 +1000

    Updated dialog interactions for page2

--- builds/development/js/scripts2.js
+++ builds/development/js/scripts2.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   // Scripts for page2.html
+  $('#d1').click(function(){
+    $('#d1').fadeOut('Slow');
+  });
+
   document.getElementById('next-btn-2').onclick = function() {
     location.href = "page3.html";
   };
