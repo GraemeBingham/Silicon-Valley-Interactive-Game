828b44f83f9e5b09854fa58fd603f00c2bea2220
GraemeBingham <binghamgraeme@gmail.com>
2016-06-06 20:08:24 +1000

    Moved head script link to below Jquery

--- builds/development/js/scripts.js
+++ builds/development/js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
   // Scripts for Index.html
-  $('.container').fadeIn(3000,function(){
-    $('.stage').fadeIn(4000).removeClass('hidden');
+  $('.container').fadeIn(1000,function(){
+    $('.stage').fadeIn(2000).removeClass('hidden');
   }).removeClass('hidden');
 
-
+  $('.menu a').on('click',function(e){
+    $('.storypopup').fadeIn(2000).removeClass('hidden');
+  });
 
   // Scripts for page1.html
 });
