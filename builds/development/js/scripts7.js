f8222d4f9add07a16837e8839d529f83e564e5f9
GraemeBingham <binghamgraeme@gmail.com>
2016-06-08 05:25:48 +1000

    Updated interactions for page7.haml

---
+++ builds/development/js/scripts7.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  // Scripts for page7.html
+
+  $('#d1').click(function(){
+    $('#d2').removeClass('hidden')
+    $('#img-2').removeClass('hidden')
+    $('.decison').fadeIn('slow').removeClass('hidden')
+  });
+
+
+  document.getElementById('peter').onclick = function() {
+    location.href = "page8.html";
+  };
+
+  document.getElementById('peter').onclick = function() {
+    location.href = "page9.html";
+  };
+
+});
