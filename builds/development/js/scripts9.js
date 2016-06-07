caa9000b3034e1c89fefc5458da95ea95eabc2f4
GraemeBingham <binghamgraeme@gmail.com>
2016-06-08 06:39:05 +1000

    Finished interactions for page9.haml

--- builds/development/js/scripts9.js
+++ builds/development/js/scripts9.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
-  // Scripts for page7.html
+  // Scripts for page9.html
 
   $('#d1').click(function(){
     $('#d2').removeClass('hidden')
     $('#img-2').removeClass('hidden')
-    $('#next-btn-8').removeClass('hidden')
   });
 
+  $('#d2').click(function(){
+    $('#d3').removeClass('hidden')
+    $('#img-3').removeClass('hidden')
+  });
 
-  document.getElementById('next-btn-8').onclick = function() {
-    location.href = "index.html";
+  $('#d3').click(function(){
+    $('#d4').removeClass('hidden')
+    $('#img-4').removeClass('hidden')
+    $('#next-btn-9').removeClass('hidden')
+  });
+
+  document.getElementById('next-btn-9').onclick = function() {
+    location.href = "page10.html";
   };
 });
