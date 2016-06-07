a523671ddea573d33a8fa2c43ba4871b07ee663d
GraemeBingham <binghamgraeme@gmail.com>
2016-06-07 12:37:07 +1000

    Finished scripts for page4

--- builds/development/js/scripts4.js
+++ builds/development/js/scripts4.js
@@ -1,13 +1,8 @@
 $(document).ready(function(){
   // Scripts for page3.html
-
-  $('#d1').click(function(){
-    $('#d2').removeClass('hidden')
-    $('#img-2').removeClass('hidden')
+  $('#video').on('ended', function () {
+    $('#d1').fadeIn('slow').removeClass('hidden');
+    $('#').fadeIn('slow').removeClass('hidden');
+    $('#next-btn-4').removeClass('hidden')
   });
-
-
-  document.getElementById('next-btn-3').onclick = function() {
-    location.href = "page4.html";
-  };
 });
