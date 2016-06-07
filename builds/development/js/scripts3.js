991c3df176c29400b43b74b78cd108c83f97aad8
GraemeBingham <binghamgraeme@gmail.com>
2016-06-07 11:06:07 +1000

    Finished interactions for page3

--- builds/development/js/scripts3.js
+++ builds/development/js/scripts3.js
@@ -3,28 +3,35 @@ $(document).ready(function(){
 
   $('#d1').click(function(){
     $('#d2').removeClass('hidden')
+    $('#img-2').removeClass('hidden')
   });
 
   $('#d2').click(function(){
     $('#d3').removeClass('hidden')
+    $('#img-3').removeClass('hidden')
   });
 
   $('#d3').click(function(){
     $('#d4').removeClass('hidden')
+    $('#img-4').removeClass('hidden')
   });
 
   $('#d4').click(function(){
     $('#d5').removeClass('hidden')
+    $('#img-5').removeClass('hidden')
   });
 
   $('#d5').click(function(){
     $('#d6').removeClass('hidden')
+    $('#img-6').removeClass('hidden')
   });
 
-  $('#d5').click(function(){
-    $('#d6').removeClass('hidden')
+  $('#d6').click(function(){
+    $('#d7').removeClass('hidden')
+    $('#img-7').removeClass('hidden')
     $('#next-btn-3').removeClass('hidden')
   });
+
 
   document.getElementById('next-btn-3').onclick = function() {
     location.href = "page4.html";
