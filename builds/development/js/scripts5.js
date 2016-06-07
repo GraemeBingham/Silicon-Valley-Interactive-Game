b8fd31eb0d4c7acf6f5777d553059d2aaae5e5b8
GraemeBingham <binghamgraeme@gmail.com>
2016-06-07 13:16:57 +1000

    Finished interactions on page5.haml

--- builds/development/js/scripts5.js
+++ builds/development/js/scripts5.js
@@ -1,39 +1,13 @@
 $(document).ready(function(){
-  // Scripts for page3.html
+  // Scripts for page5.html
 
   $('#d1').click(function(){
     $('#d2').removeClass('hidden')
     $('#img-2').removeClass('hidden')
+    $('#next-btn-5').removeClass('hidden')
   });
 
-  $('#d2').click(function(){
-    $('#d3').removeClass('hidden')
-    $('#img-3').removeClass('hidden')
-  });
-
-  $('#d3').click(function(){
-    $('#d4').removeClass('hidden')
-    $('#img-4').removeClass('hidden')
-  });
-
-  $('#d4').click(function(){
-    $('#d5').removeClass('hidden')
-    $('#img-5').removeClass('hidden')
-  });
-
-  $('#d5').click(function(){
-    $('#d6').removeClass('hidden')
-    $('#img-6').removeClass('hidden')
-  });
-
-  $('#d6').click(function(){
-    $('#d7').removeClass('hidden')
-    $('#img-7').removeClass('hidden')
-    $('#next-btn-3').removeClass('hidden')
-  });
-
-
-  document.getElementById('next-btn-3').onclick = function() {
-    location.href = "page4.html";
+  document.getElementById('next-btn-5').onclick = function() {
+    location.href = "page6.html";
   };
 });
